beef8377ff5814036a62a0b71207acc49d191870
AndreBorelli <71100458+AndreBorelli@users.noreply.github.com>
2022-06-20 13:40:10 -0300

    Update 2-useState-basics.js

--- 2-useState-basics.js
+++ 2-useState-basics.js
@@ -1,3 +1,7 @@
+// In this project we use useState, assigning the initial value as a string with the value "random title" and we create a handleClick function that uses the "if" 
+// to check if the title is the same or different from the one passed in the function and changing it according to the value that it is dynamically presenting, making 
+// it have two possible values.
+
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
